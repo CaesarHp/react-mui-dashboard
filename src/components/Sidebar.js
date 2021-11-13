693ad37efd5d1c451a7da116282a107c2ba3628c
CaesarHp <hp88502225@gmail.com>
2021-11-12 22:22:34 -0500

    side-bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import HomeIcon from "@mui/icons-material/Home";
+import PieChartIcon from "@mui/icons-material/PieChart";
+import ViewListIcon from "@mui/icons-material/ViewList";
+import PersonIcon from "@mui/icons-material/Person";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 function Sidebar() {
-  return <div></div>;
+  const classes = useStyles();
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const listChangeHandler = (event, index) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <>
+      <div className={classes.root}>
+        <List component="nav">
+          <ListItem
+            button
+            selected={selectedIndex === 0}
+            onClick={(event) => listChangeHandler(event, 0)}
+          >
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem
+            button
+            selected={selectedIndex === 1}
+            onClick={(event) => listChangeHandler(event, 1)}
+          >
+            <ListItemIcon>
+              <PieChartIcon />
+            </ListItemIcon>
+            <ListItemText primary="Analysis" />
+          </ListItem>
+          <ListItem
+            button
+            selected={selectedIndex === 2}
+            onClick={(event) => listChangeHandler(event, 2)}
+          >
+            <ListItemIcon>
+              <ViewListIcon />
+            </ListItemIcon>
+            <ListItemText primary="List" />
+          </ListItem>
+          <ListItem
+            button
+            selected={selectedIndex === 3}
+            onClick={(event) => listChangeHandler(event, 3)}
+          >
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary="Account" />
+          </ListItem>
+        </List>
+      </div>
+    </>
+  );
 }
 
 export default Sidebar;
--- src/ui/Layout.js
+++ src/ui/Layout.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 import { Grid } from "@mui/material";
+import { Paper } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core";
+
+import logo from "../local-img/logo.png";
+import promot1 from "../local-img/promot-1.png";
+import Sidebar from "../components/Sidebar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,6 +17,45 @@ const useStyles = makeStyles((theme) => ({
     backgroundSize: "cover",
     height: "100vh",
     overflow: "hidden",
+  },
+  container: {
+    margin: "3rem 10rem",
+  },
+  logoContainer: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    padding: "2rem 1rem",
+  },
+  logoImg: {
+    width: "2rem",
+    height: "2rem",
+  },
+  title: {
+    fontWeight: 600,
+    marginLeft: "1rem",
+  },
+  promotContainer: {
+    display: "flex",
+    flexDirection: "column",
+    padding: "2rem 1rem",
+  },
+  promotImg: {
+    width: "8rem",
+    height: "8rem",
+  },
+  btn: {
+    borderRadius: "5px",
+    padding: "0.8rem",
+    backgroundColor: "black",
+    fontWeight: 500,
+    textTransform: "none",
+    color: "white",
+    marginTop: "1rem",
+
+    "&:hover": {
+      backgroundColor: theme.palette.grey[900],
+    },
   },
 }));
 
@@ -19,10 +65,50 @@ function Layout({ children }) {
   return (
     <>
       <div className={classes.root}>
-        <Grid container>
-          <Grid></Grid>
-          <Grid item>{children}</Grid>
-        </Grid>
+        <Paper elevation={1} className={classes.container}>
+          <Grid container>
+            <Grid item xs={2}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <div className={classes.logoContainer}>
+                    <img alt="logo" src={logo} className={classes.logoImg} />
+                    <Typography
+                      variant="h5"
+                      component="h1"
+                      className={classes.title}
+                    >
+                      Tech-Fit
+                    </Typography>
+                  </div>
+                </Grid>
+                <Grid item xs={12}>
+                  <Sidebar />
+                </Grid>
+                <Grid item xs={12}>
+                  <div className={classes.promotContainer}>
+                    <img
+                      alt="logo"
+                      src={promot1}
+                      className={classes.promotImg}
+                    />
+                    <div>
+                      <Button
+                        variant="contained"
+                        disableElevation
+                        className={classes.btn}
+                      >
+                        Upgrade to Pro
+                      </Button>
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={10}>
+              {children}
+            </Grid>
+          </Grid>
+        </Paper>
       </div>
     </>
   );
